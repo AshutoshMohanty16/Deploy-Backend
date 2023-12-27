34bd87c8a9ddb0a82875c73ff1b9187b6d2b8747
AshutoshMohanty16 <ashutoshmohantysipu@gmail.com>
2023-12-27 14:19:34 +0530

    Initial commit

---
+++ src/test/java/com/project/DeployBackend/DeployBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.DeployBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
